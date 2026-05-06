5c24c3422ae8c4c20cc343f281a67b824f63675c
abiralkhatiwada <abiralkhatiwada37@gmail.com>
2026-05-06 12:58:34 +0545

    logo for urls changed

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic.base import RedirectView
 from portfolio.sitemaps import StaticViewSitemap, BlogSitemap
 
 sitemaps = {
@@ -14,6 +15,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'images/logo.png')),
 ]
 
 # Serve media files in development
