3fef24a85be9cbf0b26eb1fae49b4125cc56c88c
abiralkhatiwada <abiralkhatiwada37@gmail.com>
2026-08-04 20:26:21 +0545

    fix: switch vercel.json from rewrites to routes+builds to fix PATH_INFO routing

--- portfolio_site/wsgi.py
+++ portfolio_site/wsgi.py
@@ -4,4 +4,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_site.settings')
 
 application = get_wsgi_application()
-app = application
+app = application
